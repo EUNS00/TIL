ea179e9e74ba9b2a36bb018301628c27a50b6cf9
eunsoo <leeus5629@naver.com>
2019-07-09 14:23:51 +0900

    first commit

---
+++ 00_StartCamp/01_Day/00_auto_browser.py
@@ -0,0 +1,12 @@
+import webbrowser
+#import random
+
+#a = ['레드벨벳', 'bts', '샤이니']
+#b = random.choice(a)
+#for b in a
+
+#webbrowser.open('https://google.com')
+#webbrowser.open_new('https://google.com')
+#webbrowser.open_new_tab('https://google.com')
+#webbrowser.open('https://search.naver.com/search.naver?query=' + b)
+
